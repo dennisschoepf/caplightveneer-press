6c4db87e5ee87267d33c3b739b4b4187fabf11a9
Dennis Schoepf <dennis@schoepf.co>
2020-09-11 16:19:28 +0200

    Testing interaction

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,35 @@
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define SENSOR_PIN 2
+#define LED_PIN 3
+#define LED_COUNT 24
+
+bool isTouched = false;
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+void setup()
+{
+  Serial.begin(9600);
+  strip.begin();
+  strip.show();
+  strip.setBrightness(50);
+  pinMode(SENSOR_PIN, INPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  isTouched = digitalRead(SENSOR_PIN);
+
+  if (isTouched == true)
+  {
+    Serial.println("Is touched");
+    strip.setPixelColor(0, 255, 0, 0);
+    strip.show();
+  }
+  else
+  {
+    strip.clear();
+    strip.show();
+  }
 }
